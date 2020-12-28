1914f584d38fdddcec8ef3e83953c972a12bd83e
bobstrange <bob.1983.g@gmail.com>
2020-12-28 23:25:55 +0900

    feat: marshal example

---
+++ json_example/marshal_test.go
@@ -0,0 +1,31 @@
+package json_example_test
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+type ColorGroup struct {
+	ID     int
+	Name   string
+	Colors []string
+}
+
+func TestMarshal(t *testing.T) {
+	group := ColorGroup{
+		ID:   1,
+		Name: "Reds",
+		Colors: []string{
+			"Crimson",
+			"Red",
+			"Ruby",
+			"Maroon",
+		},
+	}
+	b, err := json.Marshal(group)
+	if err != nil {
+		t.Fatal("This shouldn't return error but got", err)
+	}
+	os.Stdout.Write(b)
+}
