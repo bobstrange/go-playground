73d076f94c3d011553c3aae6be11eb678cbbcdfb
bobstrange <bob.1983.g@gmail.com>
2022-11-05 00:42:09 +0900

    chore: you'd better add detail to error

---
+++ practical_go/5_2_handle_error.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type User any
+
+func getInvitedUserWithEmail(ctx context.Context, email string) (*User, error) {
+	var user *User
+	return user, nil
+}
+
+func badHandling() (*User, error) {
+	var email string
+	user, err := getInvitedUserWithEmail(context.TODO(), email)
+	if err != nil {
+		// 発生したエラーをそのまま呼び出し元に返却
+		return nil, err
+	}
+	return user, nil
+}
+
+func goodHandling() (*User, error) {
+	var email string
+	user, err := getInvitedUserWithEmail(context.TODO(), email)
+	if err != nil {
+		// 呼び出し元で発生したエラーをラップし、付加情報を付与して呼び出し元に返却
+		return nil, fmt.Errorf("fail to get invited user with email (%s): %w", email, err)
+	}
+	return user, nil
+}
