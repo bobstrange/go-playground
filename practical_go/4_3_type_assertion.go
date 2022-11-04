01a2a1ed48233258eaf626d5bd60c93c8d5d53bc
bobstrange <bob.1983.g@gmail.com>
2022-11-04 23:16:37 +0900

    feat: upcast and downcast for slices

--- practical_go/4_3_type_assertion.go
+++ practical_go/4_3_type_assertion.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 )
 
@@ -22,4 +23,33 @@ func main() {
 	default:
 		log.Printf("I prefer %v.\n", v)
 	}
+
+	var r any
+	// interface を満たすかどうかの assertion
+	if c, ok := r.(io.Closer); ok {
+		c.Close()
+	}
+
+	type Fish any
+	var fishList = []Fish{"サバ", "ブリ", "マグロ"}
+	// ↓ の様に一括で downcast はできない
+	// var fishNameList = fishList.([]string)
+
+	// any への一括 upcast もできない
+	// var anyList []any = fishList
+
+	fishNames := make([]string, len(fishList))
+	for i, f := range fishList {
+		// それぞれを型チェックして downcast する必要がある
+		if fn, ok := f.(string); ok {
+			fishNames[i] = fn
+		}
+	}
+
+	anyList := make([]any, len(fishList))
+	for i, fn := range fishList {
+		// upcast は型チェック不要
+		anyList[i] = fn
+	}
+
 }
