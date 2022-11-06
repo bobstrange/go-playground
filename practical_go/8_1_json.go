84b0779ce3fdff8eca30b71dd14ef687afc1d82d
bobstrange <bob.1983.g@gmail.com>
2022-11-07 01:07:00 +0900

    chore: decode json

---
+++ practical_go/8_1_json.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type ip struct {
+	Origin string `json:"origin"`
+	URL    string `json:"url"`
+}
+
+func main() {
+	f, err := os.Open("ip.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var resp ip
+	if err := json.NewDecoder(f).Decode(&resp); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", resp)
+	// {Origin:255.255.255.255 URL:https://httpbin.org/get}
+}
