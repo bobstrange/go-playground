756d1698c85d3438a42b5125f49eba5f9919dc10
bobstrange <bob.1983.g@gmail.com>
2022-11-24 00:15:03 +0900

    chore: create a simple server use net/http

---
+++ practical_go/10_2_server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Comment struct {
+	Message  string `json:"message"`
+	UserName string `json:"user_name"`
+}
+
+func main() {
+	mutex := &sync.RWMutex{}
+	comments := make([]Comment, 0, 100)
+
+	http.HandleFunc("/comments", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		switch r.Method {
+		case http.MethodGet:
+			mutex.RLock()
+
+			if err := json.NewEncoder(w).Encode(comments); err != nil {
+				http.Error(w, fmt.Sprintf(`{"status":"%s"}`, err), http.StatusInternalServerError)
+				return
+			}
+			mutex.RUnlock()
+		case http.MethodPost:
+			var c Comment
+			if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+				http.Error(w, fmt.Sprintf(`{"status":"%s"}`, err), http.StatusInternalServerError)
+				return
+			}
+			mutex.Lock()
+			comments = append(comments, c)
+			mutex.Unlock()
+
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte(`{"status":"created"}`))
+		default:
+			http.Error(w, `{"status":"method not allowed"}`, http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
