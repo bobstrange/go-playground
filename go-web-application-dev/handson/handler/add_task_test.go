e4ba42e8b32c7989ec5c2f076df55e295a917077
bobstrange <bob.1983.g@gmail.com>
2022-12-30 23:24:20 +0900

    feat: add test for add_task

---
+++ go-web-application-dev/handson/handler/add_task_test.go
@@ -0,0 +1,63 @@
+package handler_test
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bobstrange/go-playground/go-web-application-dev/handson/entity"
+	"github.com/bobstrange/go-playground/go-web-application-dev/handson/handler"
+	"github.com/bobstrange/go-playground/go-web-application-dev/handson/store"
+	"github.com/bobstrange/go-playground/go-web-application-dev/handson/testutil"
+	"github.com/go-playground/validator/v10"
+)
+
+func TestAddTask(t *testing.T) {
+	t.Parallel()
+
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		reqFile string
+		want    want
+	}{
+		"ok": {
+			reqFile: "testdata/add_task/ok_req.json.golden",
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/add_task/ok_rsp.json.golden",
+			},
+		},
+		"badRequest": {
+			reqFile: "testdata/add_task/bad_req.json.golden",
+			want: want{
+				status:  http.StatusBadRequest,
+				rspFile: "testdata/add_task/bad_req_rsp.json.golden",
+			},
+		},
+	}
+	for n, tt := range tests {
+		tt := tt
+
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodPost, "/tasks", bytes.NewReader(testutil.LoadFile(t, tt.reqFile)))
+
+			sut := handler.AddTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+				Validator: validator.New(),
+			}
+			sut.ServeHTTP(w, r)
+			resp := w.Result()
+			testutil.AssertResponse(t, resp, tt.want.status, tt.want.rspFile)
+		})
+
+	}
+}
